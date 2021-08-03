dcad514f62d73188d255842dafcd72c12aac6832
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2021-08-03 07:06:46 +0530

    Mobile friendliness added

--- src/App.js
+++ src/App.js
@@ -9,27 +9,21 @@ function App() {
   const [MD, SetMD] = useState(`
 # Welcome to MD Editor.
 
-
 This editor is developed by Devang Saklani from **INDIA**  ( as a side project ).
-
-
+ 
 *Basic usage:*
 # Heading 1
 ## Heading 2
 ### Heading 3
 #### Heading 4
 
-
 *This text will be italic*
 _This will also be italic_
-
 
 **This text will be bold**
 __This will also be bold__
 
-
 _You **can** combine them_
-
 
 ## List
 * Item 1
@@ -50,12 +44,9 @@ First Header | Second Header
 Content from cell 1 | Content from cell 2
 Content in the first column | Content in the second column
 
-
 It's very easy to make some words **bold** and other words *italic* with Markdown. You can even [link a Website!](https://devang-saklani.web.app)
 
-
 ### Finally some lorem Ipsum
-
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin gravida eros nec mollis sodales. Proin eu rutrum nisi, at feugiat neque. Sed sit amet quam vel justo maximus efficitur et id mauris. Pellentesque ultrices metus at gravida tincidunt. Nulla placerat pretium nulla. Proin ultricies magna ut sem pharetra, non blandit erat bibendum. Pellentesque risus justo, placerat nec nibh at, feugiat faucibus lorem. Maecenas tincidunt dolor ex, eu ultricies velit tempor nec. Suspendisse fermentum pretium tristique. Etiam eu lectus massa. Suspendisse est metus, dictum nec iaculis a, ullamcorper quis metus. Nullam nec placerat elit. Nullam leo sem, scelerisque in augue egestas, fringilla suscipit tellus. Vestibulum a tincidunt turpis. Sed feugiat, enim quis fringilla ultricies, ante est dignissim purus, id pulvinar diam turpis et tortor. Nunc vitae odio vehicula, condimentum sapien eget, fermentum nunc. 
 `);
